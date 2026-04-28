6c6fd3fd9841c1b071cf71264d9935574ed64f65
Dcberr <khongcanbiet941612@gmail.com>
2026-04-29 00:25:14 +0700

    Fix python path err

--- movie-analytics-dashboard/pages/3_📈_Trends.py
+++ movie-analytics-dashboard/pages/3_📈_Trends.py
@@ -1,4 +1,10 @@
-import bootstrap
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+project_root_str = str(PROJECT_ROOT)
+if project_root_str not in sys.path:
+    sys.path.insert(0, project_root_str)
 
 import streamlit as st
 import plotly.express as px
